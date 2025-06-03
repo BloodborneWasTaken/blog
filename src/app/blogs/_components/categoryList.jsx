e1f1621bbc0376d0929aeae136623ca6d0b51007
BloodborneWasTaken <alijerban1388@gmail.com>
2025-06-03 20:01:32 +0330

    yay

--- src/app/blogs/_components/categoryList.jsx
+++ src/app/blogs/_components/categoryList.jsx
@@ -1,27 +1,27 @@
-import Link from "next/link"
+import Link from "next/link";
 
-async function categoryList() {
-    const res = await fetch("http://localhost:5000/api/category/list")
-    const  {
-        data : { categories },
-    } = await res.json
-    console.log(categories)
-return (
-    <div className=' space-y-4'>
+async function CategoryList() {
+  const res = await fetch("http://localhost:5000/api/category/list", {
+
+  });
+  const { data: { categories } } = await res.json();
+
+
+
+  return (
+    <ul className="space-y-4">
+      <li>
         <Link href="/blogs">همه</Link>
-        {
-            categories.map((category)=>{
-                return (
-                    <li key={category._id}>
-                        <Link href={`/blogs/category/${category.slug}`}>
-                            {category.title}
-                        </Link>
-                    </li>
-                )
-            })
-        }
-    </div>
-  )
+      </li>
+      {categories.map((category) => (
+        <li key={category._id}>
+          <Link href={`/blogs/category/${category.slug}`}>
+            {category.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default categoryList
+export default CategoryList;
